f2320b4e239c75e5140bc1075550d8a8de965556
msopanen <mikko.p.sopanen@gmail.com>
2017-04-23 21:54:37 +0300

    creating utils for reducers

---
+++ src/main/utils/StoreFactory.js
@@ -0,0 +1,23 @@
+import { createStore } from 'redux';
+
+import Reducer from "../reducers/Counter";
+
+/**
+ * HMR for reducers 
+ * https://github.com/reactjs/react-redux/releases/tag/v2.0.0
+ * 
+ * @param {*} state 
+ */
+export default function configureStore(state) {
+    const store = createStore(Reducer, state);
+    
+    if (module.hot) {
+        // Enable Webpack hot module replacement for reducers
+        module.hot.accept("../reducers/Counter", () => {
+            const nextRootReducer = require("../reducers/Counter").default;
+            store.replaceReducer(nextRootReducer);
+        });
+    }
+
+    return store;
+};
