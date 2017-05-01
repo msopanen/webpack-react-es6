a7204e917a7089f083067ab4f163d6dffa4d6dd2
msopanen <mikko.p.sopanen@gmail.com>
2017-05-01 10:34:52 +0300

    adding first tests

---
+++ src/spec/components/Counter.spec.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Counter from "../../main/components/Counter";
+
+it("renders properly", () => {
+    /*const tree = renderer.create(
+        <Counter store={} />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();*/
+})
