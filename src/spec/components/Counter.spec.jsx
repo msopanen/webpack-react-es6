b11519743b83bb38765c805bc53a4862dac3d997
msopanen <mikko.p.sopanen@gmail.com>
2017-05-01 13:05:50 +0300

    first snapshot test with redux

--- src/spec/components/Counter.spec.jsx
+++ src/spec/components/Counter.spec.jsx
@@ -1,12 +1,35 @@
 
 import React from 'react';
 import renderer from 'react-test-renderer';
+import configureMockStore from 'redux-mock-store';
 
 import Counter from "../../main/components/Counter";
 
-it("renders properly", () => {
-    /*const tree = renderer.create(
-        <Counter store={} />
+const middlewares = [];
+const mockStore = configureMockStore(middlewares);
+
+it("renders initial state 0 properly",() => {
+
+    const getState = { counter: 0 }; // initial state of the store
+
+    const store = mockStore(getState, []);
+    const tree = renderer.create(
+        <Counter store={store} />
     ).toJSON();
-    expect(tree).toMatchSnapshot();*/
-})
+    expect(tree).toMatchSnapshot();
+});
+
+/*it("renders properly", (done) => {
+
+    const getState = { counter: 0 }; // initial state of the store
+    const increment = { type: 'INCREMENT' };
+    const expectedActions = [increment];
+
+    const store = mockStore(getState, expectedActions, done);
+    const tree = renderer.create(
+        <Counter store={store} />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+
+    store.dispatch(increment);
+});*/
