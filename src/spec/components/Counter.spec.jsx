55da6142a047fa8c7b74dc9f4133d537f0fdcb70
msopanen <mikko.p.sopanen@gmail.com>
2017-05-01 17:45:28 +0300

    adding linting for tests

--- src/spec/components/Counter.spec.jsx
+++ src/spec/components/Counter.spec.jsx
@@ -1,7 +1,7 @@
 
-import React from 'react';
-import renderer from 'react-test-renderer';
-import configureMockStore from 'redux-mock-store';
+import React from "react";
+import renderer from "react-test-renderer";
+import configureMockStore from "redux-mock-store";
 
 import Counter from "../../main/components/Counter";
 
@@ -9,21 +9,19 @@ const middlewares = [];
 const mockStore = configureMockStore(middlewares);
 
 it("renders initial state 0 properly", () => {
-
     const getState = { counter: 0 }; // initial state of the store
 
     const store = mockStore(getState, []);
     const tree = renderer.create(
-        <Counter store={store} />
+      <Counter store={store} />,
     ).toJSON();
     expect(tree).toMatchSnapshot();
-
 });
 
-/*it("renders properly", (done) => {
+/* it("renders properly", (done) => {
 
     const getState = { counter: 0 }; // initial state of the store
-    const increment = { type: 'INCREMENT' };
+    const increment = { type: "INCREMENT" };
     const expectedActions = [increment];
 
     const store = mockStore(getState, expectedActions, done);
